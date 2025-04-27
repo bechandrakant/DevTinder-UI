1ce831c1c4d0a7742495b77970f96f3688a32879
Chandrakant <chandrakantsavvy@gmail.com>
2025-04-27 23:59:21 +0530

    add protected routes

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./Navbar";
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Footer from "./Footer";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+import { addUser } from "../features/userSlice";
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const { user } = useSelector((state) => state.user);
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    if (!user) {
+      axios
+        .get("http://localhost:3000/profile/view", {
+          withCredentials: true,
+        })
+        .then(({ data }) => {
+          dispatch(addUser(data));
+        })
+        .catch((err) => {
+          if (err.status === 400) {
+            navigate("/login");
+          }
+          console.error(err);
+        });
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div className="flex flex-col h-[100vh]">
       <Navbar />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { removeUser } from "../features/userSlice";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const { user } = useSelector((state) => state.user);
   const dispatch = useDispatch();
 
+  const handleLogout = () => {
+    dispatch(removeUser());
+  };
+
   return (
     <div className="navbar bg-base-300 shadow-sm">
       <div className="flex-1">
-        <a className="btn btn-ghost text-xl">👨‍💻 DevTinder</a>
+        <Link to={"/"} className="btn btn-ghost text-xl">
+          👨‍💻 DevTinder
+        </Link>
       </div>
       {user && (
         <div className="flex items-center gap-2">
@@ -29,15 +36,15 @@ const Navbar = () => {
               className="menu menu-sm dropdown-content bg-base-300 rounded-box z-1 mt-3 w-52 p-2 shadow"
             >
               <li>
-                <a className="justify-between">
+                <Link to="profile" className="justify-between">
                   Profile
                   <span className="badge">New</span>
-                </a>
+                </Link>
               </li>
               <li>
                 <a>Settings</a>
               </li>
-              <li onClick={() => dispatch(removeUser())}>
+              <li onClick={handleLogout}>
                 <a>Logout</a>
               </li>
             </ul>

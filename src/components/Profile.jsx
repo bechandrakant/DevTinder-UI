8b386e83352d64a4b13b60d2c864d46ff76ba647
Chandrakant <chandrakantsavvy@gmail.com>
2025-04-29 02:32:38 +0530

    add profile

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addUser } from "../features/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const Profile = () => {
-  return <div>Profile</div>;
+  const [email, setEmail] = useState("sachin@gmail.com");
+  const [password, setPassword] = useState("sachin123");
+
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const updateProfile = () => {
+    axios
+      .post(
+        "http://localhost:3000/login",
+        { email, password },
+        { withCredentials: true }
+      )
+      .then((response) => {
+        dispatch(addUser(response.data));
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err);
+        alert("Login failed, please try again");
+      });
+  };
+  return (
+    <div>
+      <div className="flex justify-center items-center flex-auto">
+        <div className="card card-border bg-base-300 w-96">
+          <div className="card-body">
+            <h2 className="card-title place-self-center">Update Profile</h2>
+            <div>
+              <input type="text" placeholder="Name" className="input my-2" />
+            </div>
+            <div className="card-actions place-content-center">
+              <button className="btn btn-primary" onClick={updateProfile}>
+                Save changes
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Profile;
